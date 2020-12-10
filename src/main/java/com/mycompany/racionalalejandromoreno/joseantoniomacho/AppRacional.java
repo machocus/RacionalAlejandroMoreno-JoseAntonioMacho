96493a2dc73dfa191e682c6c85d9acd7e15c9008
Moreno27 <alejanmm2000@gmail.com>
2020-12-10 17:40:06 +0100

    Añadimos souts

--- src/main/java/com/mycompany/racionalalejandromoreno/joseantoniomacho/AppRacional.java
+++ src/main/java/com/mycompany/racionalalejandromoreno/joseantoniomacho/AppRacional.java
@@ -15,16 +15,22 @@ public class AppRacional {
         Racional k = new Racional(3, 4);
         Racional ramdom = new Racional();//como es ramdon, no le hacen falta parametros
         
+        
         ramdom.aleatorio();//metodo para ramdomizar los valores del racional
         
+        System.out.println("Numeros:\n");
         z.imprimirConsola();//imprime por pantalla
         k.imprimirConsola();//imprime por pantalla
         ramdom.imprimirConsola();//imprime por pantalla
         
+        System.out.println("Igualdad:\n");
         Racional.igualdad(z, k); //Imprime por pantalla la igualdad de z entre k
         
-        z.suma(k);//suma e imprime por pantalla el resultado
+        System.out.println("Suma:\n");
+        z.suma(k).imprimirConsola();//z.suma(k) es un racional, entonces imprimimos eser racional por consola
         
+        System.out.println("Division:\n");
         Racional division = z.division(k);
         division.imprimirConsola();
+    }
 }
