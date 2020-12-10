20094c9fd693ad274b11bd2f775f646b6e1df317
Moreno27 <alejanmm2000@gmail.com>
2020-12-10 13:43:52 +0100

    Cambio de nombre de clase. Generada la clase de prueba. Creado el metodo de suma y el toString

---
+++ src/main/java/com/mycompany/racionalalejandromoreno/joseantoniomacho/Racional.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racionalalejandromoreno.joseantoniomacho;
+
+/**
+ *
+ * @author aguil
+ */
+public class Racional {
+
+    private int numerador;
+    private int denominador;
+
+    public Racional() {
+        this.numerador = 1;
+        this.denominador = 1;
+    }
+
+    public Racional(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+
+        if (denominador == 0) {
+            this.denominador = 1;
+        }
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public void imprimirConsola() {
+        System.out.println("Número Relacional " + numerador + "/" + denominador);
+    }
+
+    @Override
+    public String toString() {
+        return numerador + "/" + denominador;
+    }
+
+    public void suma(Racional numero) {
+
+        if (numero.getDenominador() == this.denominador) {
+            this.numerador += numero.getNumerador();
+        } else {
+            //operaicon para poner el numerador de la fraccion
+            this.numerador = (this.numerador * numero.getDenominador())//esto seria el a*d
+                    + (this.denominador + numero.getNumerador());//esto seria el b*c
+            //operaciones para el denominador
+            //this.denominador = this.denominador * numero.getDenominador();
+            //se resume en:
+            this.denominador *= numero.getDenominador();
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/racionalalejandromoreno/joseantoniomacho/claseDePrueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racionalalejandromoreno.joseantoniomacho;
+
+/**
+ *
+ * @author aleja
+ */
+public class claseDePrueba {
+    public static void main(String[] args) {
+        
+        Racional numero1 = new Racional(8, 0);
+        
+        Racional numero2 = new Racional(9, 4);
+        
+        numero1.suma(numero2);
+        
+    }
+}
